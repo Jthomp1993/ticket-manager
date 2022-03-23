b1e9bfe5a18e19acf84d4f0be6091794d5b61b42
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-03-23 12:54:47 +0000

    Protect routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,6 +75,13 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+    res.status(200).json(req.user)
+})
+
 // Generate web token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -84,5 +91,6 @@ const generateToken = (id) => {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
