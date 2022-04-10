d0b86289d50f9abbe5e55a72d63e5080d885329d
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-04-10 15:54:40 +0100

    Single ticket display

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -30,9 +30,24 @@ const getTickets = async (token) => {
     return response.data;
 }
 
+
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            authorization: `Bearer ${token}`,
+        }
+    }
+
+    const response = await axios.get(API_URL + ticketId, config);
+
+    return response.data;
+}
+
 const ticketService = {
     createTicket,
     getTickets,
+    getTicket,
 }
 
 export default ticketService;
