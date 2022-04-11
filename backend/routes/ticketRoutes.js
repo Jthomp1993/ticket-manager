6d84bc9ba44a1262edff045f920bc893b084d72f
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-04-11 19:46:22 +0100

    Notes backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const { getTickets, getTicket, createTicket, updateTicket, deleteTicket } = require('../controllers/ticketController');
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes', noteRouter);
+
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 router.route('/:id').get(protect, getTicket).put(protect, updateTicket).delete(protect, deleteTicket);
 
