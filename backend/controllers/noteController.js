12189d468fb8fc6e1d823c0023e5a23ef1394994
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-04-12 12:48:02 +0100

    Notes backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 const Note = require('../models/noteModel');
+const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
 
 // @desc    Get ticket notes
@@ -27,4 +28,37 @@ const getNotes = asyncHandler(async (req, res) => {
     res.status(200).json(notes);
 })
 
-module.exports = { getNotes };
+
+// @desc    Create ticket note
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNote = asyncHandler(async (req, res) => {
+    // Get user by ID in the JWT
+    const user = await User.findById(req.user.id);
+
+    if(!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    const ticket = await Ticket.findById(req.params.ticketId);
+
+    if(ticket.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error('User not authorized');
+    }
+
+    const note = await Note.create({ 
+        ticket: req.params.ticketId,
+        text: req.body.text,
+        isStaff: false,
+        user: req.user.id 
+    });
+
+    res.status(200).json(note);
+})
+
+module.exports = { 
+    getNotes,
+    addNote,
+};
