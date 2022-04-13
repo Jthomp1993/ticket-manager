ff69322aed8490e52c2e82b644cd57eb356f9b0b
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-04-13 21:05:33 +0100

    Submit notes

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import { FaPlus } from 'react-icons/fa';
 import Modal  from 'react-modal';
 import { getTicket, closeTicket } from "../features/tickets/ticketSlice";
-import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import { getNotes, createNote, reset as notesReset } from '../features/notes/noteSlice';
 import BackButton from "../components/BackButton";
 import Spinner from '../components/Spinner';
 import NoteItem from '../components/NoteItem';
@@ -57,7 +57,7 @@ const Ticket = () => {
     const onNoteSubmit = (e) => {
         e.preventDefault();
 
-        console.log('Submit');
+        dispatch(createNote({noteText, ticketId}));
 
         closeModal();
     }
