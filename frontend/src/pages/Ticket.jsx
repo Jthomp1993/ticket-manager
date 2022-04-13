5ba46e0428705c760b5ee6646370d843683a3fac
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-04-13 12:51:15 +0100

    Display notes

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -2,12 +2,15 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+import { getTicket, closeTicket } from "../features/tickets/ticketSlice";
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
 import BackButton from "../components/BackButton";
 import Spinner from '../components/Spinner';
+import NoteItem from '../components/NoteItem';
 
 const Ticket = () => {
     const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets);
+    const { notes, isLoading: notesIsLoading } = useSelector((state) => state.notes);
 
     const navigate = useNavigate();
     const params = useParams();
@@ -20,6 +23,7 @@ const Ticket = () => {
         }
 
         dispatch(getTicket(ticketId));
+        dispatch(getNotes(ticketId));
         // eslint-disable-next-line
     }, [isError, message, ticketId])
 
@@ -30,7 +34,7 @@ const Ticket = () => {
         navigate('/tickets');
     }
 
-    if(isLoading) {
+    if(isLoading || notesIsLoading) {
         return <Spinner />
     }
 
@@ -61,7 +65,13 @@ const Ticket = () => {
                     {ticket.description}
                 </p>
             </div>
+            <h2>Notes</h2>
             </header>
+
+            {notes.map((note) => (
+                <NoteItem key={note._id} note={note} />
+            ))}
+
             {ticket.status !== 'closed' && (
                 <button onClick={onTicketClose} className='btn btn-block btn-danger'>
                     Close Ticket
